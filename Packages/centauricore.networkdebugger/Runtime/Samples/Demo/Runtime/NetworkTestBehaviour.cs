ecd7dbf7dd6663675c0aba29a6080f119cb58ee2
Centauri <centauri2442@gmail.com>
2024-02-18 00:03:59 -0600

    Moved Samples folder to Runtime

---
+++ Packages/centauricore.networkdebugger/Runtime/Samples/Demo/Runtime/NetworkTestBehaviour.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+using Centauri.Utilities;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRC.Udon.Common;
+using Random = UnityEngine.Random;
+
+namespace Centauri.NetDebug
+{
+    public class NetworkTestBehaviour : UdonSharpBehaviour
+    {
+        public UdonBehaviour TEMPTARGET;
+        
+        [UdonSynced] public int syncedValue;
+
+        public bool requestSerialize;
+
+        public float syncTime = 1f;
+
+        private void Start()
+        {
+            SendCustomEventDelayedSeconds(nameof(SerializationLoop), syncTime);
+        }
+
+        public void SerializationLoop()
+        {
+            SendCustomEventDelayedSeconds(nameof(SerializationLoop), syncTime);
+
+            if (Networking.IsOwner(gameObject))
+            {
+                if (requestSerialize)
+                {
+                    RequestSerialization();
+                }
+            }
+        }
+    }
+}
---
+++ Packages/centauricore.networkdebugger/Runtime/Samples/Demo/Runtime/TakeOwnerOfRandom.cs
@@ -0,0 +1,31 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace Centauri.NetDebug
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class TakeOwnerOfRandom : UdonSharpBehaviour
+    {
+        public GameObject[] OwnershipObjects;
+
+
+        public void Start()
+        {
+            SendCustomEventDelayedSeconds(nameof(DelayedStart), 2f);
+        }
+
+        public void DelayedStart()
+        {
+            foreach (var item in OwnershipObjects)
+            {
+                if (Random.Range(0f, 100f) < 50f)
+                {
+                    Networking.SetOwner(Networking.LocalPlayer, item);
+                }
+            }
+        }
+    }
+}
